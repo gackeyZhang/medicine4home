7a3c686ee2bd527f0dd65f0b4ff382ef7b03c7f6
gackey <plus_7@outlook.com>
2019-04-27 20:18:36 +0800

    init

--- src/main/java/com/gackey/medicine/Medicine4homeApplication.java
+++ src/main/java/com/gackey/medicine/Medicine4homeApplication.java
@@ -1,8 +1,10 @@
 package com.gackey.medicine;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.gackey.medicine.mapper")
 @SpringBootApplication
 public class Medicine4homeApplication {
 
---
+++ src/main/java/com/gackey/medicine/entity/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * title: package-info.java
+ * copyright:©2018-2020 gackey. All Rights Reserved.
+ */
+
+/**
+ * @desc: TODO
+ * @className: package-info
+ * @author: gackey
+ * @date: 2019-04-27 20:14
+ */
+package com.gackey.medicine.entity;
