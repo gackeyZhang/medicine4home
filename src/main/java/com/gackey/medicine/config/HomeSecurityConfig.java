85b9e678db00cc79866e66bf1dca9ffa6b591747
gackey <plus_7@outlook.com>
2019-12-29 22:20:00 +0800

    security config

---
+++ src/main/java/com/gackey/medicine/config/HomeSecurityConfig.java
@@ -0,0 +1,27 @@
+/**
+ * title: HomeSecurityConfig.java
+ * copyleft:©2019-2020 gackey. All Rights Reserved.
+ */
+package com.gackey.medicine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @desc Security Config
+ * @className HomeSecurityConfig
+ * @author gackey
+ * @date 2019-12-29 22:03
+ */
+@Configuration
+public class HomeSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and()
+                .authorizeRequests().anyRequest()
+                .authenticated();
+    }
+
+}
